ed8b16a7b64b08cdddce478dbf3daeaf952d6fa2
DanielJEfres <da947619@ucf.edu>
2026-02-09 15:48:42 -0500

    slight refactor on login.php to address current ERD setup, and some error handling

--- api/login.php
+++ api/login.php
@@ -5,14 +5,18 @@ require_once 'utils.php';
 
 $inData = json_decode(file_get_contents('php://input'), true);
 
-$login = $inData['login'];
-$password = $inData['password'];
+$username= $inData['username'] ?? null;
+$password = $inData['password'] ?? null;
+
+if (!$username || !$password) {
+    sendResponse(false, "Missing username or password");
+}
 
 try {
     $conn = getDB();
 
-    $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login = ?");
-    $stmt->execute([$login]);
+    $stmt = $conn->prepare("SELECT ID, Password FROM Users WHERE Username = ?");
+    $stmt->execute([$username]);
     
     if ($stmt->rowCount() == 0) {
         sendResponse(false, "Login/Password incorrect");
@@ -24,9 +28,7 @@ try {
     if (password_verify($password, $row['Password'])) {
         
         $responseData = [
-            "id" => $row['ID'],
-            "firstName" => $row['FirstName'],
-            "lastName" => $row['LastName']
+            "username" => $username
         ];
 
         sendResponse(true, "Login successful", $responseData);
@@ -36,6 +38,6 @@ try {
     }
 
 } catch (PDOException $e) {
-    sendResponse(false, "Login error: " . $e->getMessage());
+    sendResponse(false, "Login error: " . $e->getMessage()); // reminder: remove db error details in final release
 }
 ?>
